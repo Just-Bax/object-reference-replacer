6a9fc38cba5c920ff2459be19c09f66edfb9af63
Isfandiyor Baxtiyorov <isfandiyorbaxtiyorov132@gmail.com>
2024-07-18 01:41:08 +0500

    [FEATURE] Added support for the module prefix

--- js/script.js
+++ js/script.js
@@ -55,12 +55,27 @@ function replace_sql(sql) {
     var matches = sql.match(/\b\d+\b/g);
     if ( matches ) {
         const id_get = document.getElementById('id-get').checked;
+        const prefix = document.getElementById('prefix').value.toLowerCase()
         const object_reference_ids = new Set(matches);
         for (const objRef of object_reference_data) {
             const object_reference_id = objRef['OBJ_ID'];
-            const object_reference_name = objRef['OBJ_REF'];
+            const object_type = objRef['OBJ_TYPE'];
+            var object_reference_name = objRef['OBJ_REF'];
             if (object_reference_ids.has(object_reference_id)) {
                 const regex = new RegExp(`\\b${object_reference_id}\\b`, 'g');
+                if (prefix) {
+                    if (object_type === 'Fields') {
+                        object_reference_name = object_reference_name.replace('id.', `id.${prefix}_`);
+                        object_reference_name = object_reference_name.replace('.cf.', `.cf.${prefix}_`);
+                    } else if (object_type === 'Trackor Tree') {
+                        object_reference_name = object_reference_name.replace('id.', `id.${prefix}_`);
+                        object_reference_name = object_reference_name.replace('.rel.', `.rel.${prefix}_`);
+                    } else if (object_type === 'Trackor Types') {
+                        object_reference_name = object_reference_name.replace('id.', `id.${prefix}_`);
+                    } else if (object_type === 'Validation Tables') {
+                        object_reference_name = object_reference_name.replace('.vt.', `.vt.${prefix}_`);
+                    }
+                }
                 sql = sql.replace(regex, id_get ? `id.get('${object_reference_name}')` : object_reference_name);
             }
         }
